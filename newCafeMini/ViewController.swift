7ab211b6e933be1a315229227f71941f8b3749d6
noelortiz <noelo12130613@gmail.com>
2023-09-22 09:18:25 -0500

    stretch and mvp

--- newCafeMini/ViewController.swift
+++ newCafeMini/ViewController.swift
@@ -16,9 +16,18 @@ class ViewController: UIViewController,UITextFieldDelegate {
     @IBOutlet weak var total: UILabel!
     @IBOutlet weak var inCorrectlLbl: UILabel!
     @IBOutlet weak var quanitit: UITextField!
+    
+    @IBOutlet weak var sedretField: UITextField!
+    
+    @IBOutlet weak var bossyBoy: UILabel!
+    
+    @IBOutlet weak var urItem: UITextField!
+    
+    @IBOutlet weak var urPrice: UITextField!
+    
     var addit = ""
     var yum = ""
-    
+    var answer = ""
     override func viewDidLoad() {
         super.viewDidLoad()
         for i in 0..<food.count{
@@ -26,32 +35,79 @@ class ViewController: UIViewController,UITextFieldDelegate {
             let pric = prices[i]
             yum += "\n\(menui), $\(pric)"
             munchMenu.text = yum
+            
+            urItem.delegate = self
+            quanitit.delegate = self
+            urPrice.delegate = self
+            munchField.delegate = self
+            
+            
+            
+            
+            
         }
     }
     var food = ["HotDog", "Cheese Burger" , "Chicken" , "Salad","Pretzels"]
         var prices =  [3.50,4.00,3.75,3.00,2.00]
+    var clicked = -1
+    var i = 1
     
+    @IBAction func addtoMenu(_ sender: UIButton) {
+        
+            answer = sedretField.text!
+            var itemAdd = urItem.text!
+        var priceAdd = urPrice.text!
+            if answer == "yes"{
+                yum += "\n\(itemAdd), $\(priceAdd)"
+                munchMenu.text = yum
+                food.append(itemAdd)
+                prices.append(Double(priceAdd)!)
+            }
+            
+        }
     
+
     @IBAction func addMunch(_ sender: Any) {
-        var munch = munchField.text!
-        var quantity = quanitit.text!
-        var cart = [munch : quantity]
-      
-       
-        if food.contains(munch){
-            addit += "\n\(cart)"
-            cartView.text = addit
-            let indexof = Int(exactly: food.firstIndex(of: munch)!)!
-            var prp = prices[indexof]
-            var quant = Double(quantity)
-            var yessuh =  prp * quant!
-            total.text = "$\(yessuh)"
+     
+        if clicked < i {
+            var munch = munchField.text!
+            var quantity = quanitit.text!
+            var cart = [munch : quantity]
+            clicked += 1
+            print(clicked)
+            
+            if food.contains(munch){
+                addit += "\n\(cart)"
+                cartView.text = addit
+                let indexof = Int(exactly: food.firstIndex(of: munch)!)!
+                var prp = prices[indexof]
+                var quant = Double(quantity)
+                let yessuh =  prp * quant!
+                total.text = "$\(yessuh)"
+                if cart.keys.contains(munch){
+                    inCorrectlLbl.text = "You already have this!"
+                }
+            }
+            else{
+                inCorrectlLbl.text = "Not On Menu"
+            }
+           
+        }
+    
+    }
+    
+    @IBAction func answerButt(_ sender: UIButton) {
+        answer = sedretField.text!
+        if answer == "yes"{
+            bossyBoy.text = "Only right answer"
         }
         else{
-            inCorrectlLbl.text = "Not On Menu"
+            bossyBoy.text = "try again"
         }
-        
     }
+    
+    
+    
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
